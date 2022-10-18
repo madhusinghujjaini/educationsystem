47c25ee3cdc46832a6d9e64098705e59b604d613
madhusinghujjaini <madhu08aug@gmail.com>
2022-10-18 18:45:52 +0530

    added login form ui

--- clientapp/education-system-ui/src/login/login.jsx
+++ clientapp/education-system-ui/src/login/login.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import './login.css'
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:7000/api/user/signup", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email, password: password})
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    alert(result.message)
+                })
+        alert(`The name you entered was:  ${email} ${password}`)
+    }
+
+    
     return (
-        <form>
+        <form  onSubmit={handleSubmit}>
             <h3>Login Here</h3>
             <label for="username">Username</label>
-            <input type="text" placeholder="Email or Phone" id="username" />
+            <input type="text" placeholder="Email or Phone" id="username" 
+              onChange={(e) => setEmail(e.target.value)}/>
+
             <label for="password">Password</label>
-            <input type="password" placeholder="Password" id="password" />
+            <input type="password" placeholder="Password" id="password"  
+            onChange={(e) => setPassword(e.target.value)}  />
             <button>Log In</button>
             <button>sinup</button>
 
--- clientapp/education-system-ui/src/registration/registration.jsx
+++ clientapp/education-system-ui/src/registration/registration.jsx
@@ -1,28 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import './registration.css';
 function Registration() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:7000/api/user/signup", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: name, email: email, mobile: mobile, password: password})
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    alert(result.message)
+                })
+        alert(`The name you entered was: ${name} ${email} ${mobile}`)
+    }
+
+
     return (
-        <form className="reg-form">
+        <form className="reg-form" onSubmit={handleSubmit}>
             <h3>registration form</h3>
             <div className="container">
                 <lable for="name">name</lable>
-                <input type="text" placeholder=" name" />
+                <input type="text" placeholder=" name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
             </div>
 
             <div className="container">
                 <lable for="email" >Email</lable>
-                <input type="email" placeholder="email" />
+                <input type="email" placeholder="email" value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="container">
                 <lable for="mobileno">mobileno</lable>
-                <input type="number" placeholder="mobile no" />
+                <input type="number" placeholder="mobile no" value={mobile}
+                    onChange={(e) => setMobile(e.target.value)} />
             </div>
             <div className="container">
                 <lable for="password" >password</lable>
-                <input type="password" placeholder="password" />
+                <input type="password" placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
             </div>
-        <button>submit</button>
-        </form >
+            <input type='submit' />        </form >
     )
 
 
